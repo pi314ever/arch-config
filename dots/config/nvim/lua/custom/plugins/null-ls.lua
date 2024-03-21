a9d8a0b907a4d5f79f726fe9038d304257c9cfd0
pi314ever <pilotflyer824@gmail.com>
2024-03-20 22:30:23 -0700

    Updated nvim config

--- dots/config/nvim/lua/custom/plugins/null-ls.lua
+++ dots/config/nvim/lua/custom/plugins/null-ls.lua
@@ -3,16 +3,6 @@ return {
   requires = { "nvim-lua/plenary.nvim" },
 
   config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- null_ls.builtins.formatting.black,
-        -- null_ls.builtins.formatting.isort,
-        -- null_ls.builtins.formatting.clang_format,
-        -- null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.formatting.latexindent,
-      },
-    })
     vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "[F]or[m]at document" })
   end,
 
--- dots/config/nvim/lua/custom/plugins/oil.lua
+++ dots/config/nvim/lua/custom/plugins/oil.lua
@@ -1,17 +1,28 @@
 return {
-  "pi314ever/oil.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup {
-      default_file_explorer = true,
-      columns = {
-        "icon",
-        "permissions",
-        "size",
-        "mtime"
-      },
+  -- "pi314ever/oil.nvim",
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        default_file_explorer = true,
+        columns = {
+          'icon',
+          'permissions',
+          'size',
+          'mtime',
+        },
+        win_options = {
+          signcolumn = 'yes:2',
+        },
+      }
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
 
-    }
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end,
+  {
+    'refractalize/oil-git-status.nvim',
+    dependencies = { 'stevearc/oil.nvim' },
+    config = true,
+  },
 }
