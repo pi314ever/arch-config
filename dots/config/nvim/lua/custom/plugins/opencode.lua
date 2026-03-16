8d5bc3a4e87ae06ae0c0bd394d7722cd1a09de34
Daniel Huang <pilotflyer824@gmail.com>
2026-03-16 00:16:25 -0700

    Update opencode
    Signed-off-by: Daniel Huang <pilotflyer824@gmail.com>

--- dots/config/nvim/lua/custom/plugins/opencode.lua
+++ dots/config/nvim/lua/custom/plugins/opencode.lua
@@ -9,13 +9,16 @@ return {
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      provider = {
-        enabled = 'snacks',
-        snacks = {
-          win = {
-            enter = true, -- Focus opencode terminal when opened.
-          },
-        },
+      server = {
+        start = function()
+          require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+        end,
+        stop = function()
+          require('snacks.terminal').get(opencode_cmd, snacks_terminal_opts):close()
+        end,
+        toggle = function()
+          require('snacks.terminal').toggle(opencode_cmd, snacks_terminal_opts)
+        end,
       },
     }
 
